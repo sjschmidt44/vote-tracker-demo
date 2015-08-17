68a11036a15aa99ac924a4516cf0f5e622eb46ca
Benton Green <bentongreen076@gmail.com>
2015-08-17 16:17:41 -0700

    version 2.0

--- js/app.js
+++ js/app.js
@@ -1,42 +1,45 @@
 'use strict';
 
 var photoArray = [];
+var randInt1;
+var randInt2;
 
-var Photo = function(fileLocation){ //constructor
+var Photo = function(fileLocation, votes){ //constructor
 	this.path = fileLocation;
-	this.votes;
+	this.votes = 1;
 }
 
 //instances of your photos
-var photo0 = new Photo('img/kittens/0.jpg');
-var photo1 = new Photo('img/kittens/1.jpg');
-var photo2 = new Photo('img/kittens/2.jpg');
-var photo3 = new Photo('img/kittens/3.jpg');
-var photo4 = new Photo('img/kittens/4.jpg');
-var photo5 = new Photo('img/kittens/5.jpg');
-var photo6 = new Photo('img/kittens/6.jpg');
-var photo7 = new Photo('img/kittens/7.jpg');
-var photo8 = new Photo('img/kittens/8.jpg');
-var photo9 = new Photo('img/kittens/9.jpg');
-var photo10 = new Photo('img/kittens/10.jpg');
-var photo11 = new Photo('img/kittens/11.jpg');
-var photo12 = new Photo('img/kittens/12.jpg');
-var photo13 = new Photo('img/kittens13.jpg');
+var photo0 = new Photo('img/kittens/0.jpg', 1);
+var photo1 = new Photo('img/kittens/1.jpg', 1);
+var photo2 = new Photo('img/kittens/2.jpg', 1);
+var photo3 = new Photo('img/kittens/3.jpg', 1);
+var photo4 = new Photo('img/kittens/4.jpg', 1);
+var photo5 = new Photo('img/kittens/5.jpg', 1);
+var photo6 = new Photo('img/kittens/6.jpg', 1);
+var photo7 = new Photo('img/kittens/7.jpg', 1);
+var photo8 = new Photo('img/kittens/8.jpg', 1);
+var photo9 = new Photo('img/kittens/9.jpg', 1);
+var photo10 = new Photo('img/kittens/10.jpg', 1);
+var photo11 = new Photo('img/kittens/11.jpg', 1);
+var photo12 = new Photo('img/kittens/12.jpg', 1);
+var photo13 = new Photo('img/kittens13.jpg', 1);
 
-photoArray.push(photo0, photo0.votes);
-photoArray.push(photo1, photo1.votes);
-photoArray.push(photo2, photo2.votes);
-photoArray.push(photo3, photo3.votes);
-photoArray.push(photo4, photo4.votes);
-photoArray.push(photo5, photo5.votes);
-photoArray.push(photo6, photo6.votes);
-photoArray.push(photo7, photo7.votes);
-photoArray.push(photo8, photo8.votes);
-photoArray.push(photo9, photo9.votes);
-photoArray.push(photo10, photo10.votes);
-photoArray.push(photo11, photo11.votes);
-photoArray.push(photo12, photo12.votes);
-photoArray.push(photo13, photo13.votes);
+photoArray.push(photo0);
+photoArray.push(photo1);
+photoArray.push(photo2);
+photoArray.push(photo3);
+photoArray.push(photo4);
+photoArray.push(photo5);
+photoArray.push(photo6);
+photoArray.push(photo7);
+photoArray.push(photo8);
+photoArray.push(photo9);
+photoArray.push(photo10);
+photoArray.push(photo11);
+photoArray.push(photo12);
+photoArray.push(photo13);
+console.log(photoArray)
 
 var Tracker = function(){
 	this.leftPhoto;
@@ -47,12 +50,16 @@ var Tracker = function(){
 Tracker.prototype.displayWinnerRight = function(){
 	document.getElementById("rightPhoto").style.cssText = "border: 5px solid #A786CF";
 	document.getElementById("nextButton").style.cssText = "visibility: visible;";
+    photoArray[randInt2].votes = photoArray[randInt2].votes + 1;
+    console.log("This cat has " + photoArray[randInt2].votes + " votes");
     //updateChart/votes
 }
 
 Tracker.prototype.displayWinnerLeft = function(){
 	document.getElementById("leftPhoto").style.cssText = "border: 5px solid #EF6CCD";
 	document.getElementById("nextButton").style.cssText = "visibility: visible;";
+    photoArray[randInt1].votes = photoArray[randInt1].votes + 1;
+    console.log("This cat has " + photoArray[randInt1].votes + " votes");
     //updateChart/votes
 }
 
@@ -61,8 +68,8 @@ Tracker.prototype.displayPhotos = function(){
 	document.getElementById("nextButton").style.cssText = "visibility: hidden;";
 	document.getElementById("leftPhoto").style.cssText = "border: none";
 	document.getElementById("rightPhoto").style.cssText = "border: none";
-	var randInt1 = Math.floor(Math.random() * 14);
-	var randInt2 = Math.floor(Math.random() * 14);
+	randInt1 = Math.floor(Math.random() * 14);
+	randInt2 = Math.floor(Math.random() * 14);
 	while(randInt1 == randInt2){
 		randInt2 = Math.floor(Math.random() * 14);
 	};
@@ -70,6 +77,43 @@ Tracker.prototype.displayPhotos = function(){
 	var rightPhoto = document.getElementById('rightPhoto');
 	document.getElementById("leftPhoto").src = "img/kittens/" + randInt1 + ".jpg";
 	document.getElementById("rightPhoto").src = "img/kittens/" + randInt2 + ".jpg";
+
+    this.Pie = function(data,options){
+
+            chart.Pie.defaults = {
+                segmentShowStroke : true,
+                segmentStrokeColor : "#fff",
+                segmentStrokeWidth : 2,
+                animation : true,
+                animationSteps : 100,
+                animationEasing : "easeOutBounce",
+                animateRotate : true,
+                animateScale : false,
+                percentageInnerCutout : 50,
+                onAnimationComplete : null
+
+            };      
+
+            var config = (options)? mergeChartConfig(chart.Pie.defaults,options) : chart.Pie.defaults;
+
+            return new Pie(data,config,context);                
+        };
+
+    var data = [
+
+            {
+                value: photoArray[randInt1].votes,
+                color:"#EF6CCD"
+            },
+            {
+                value : photoArray[randInt2].votes,
+                color : "#A786CF"
+            }
+
+        ];
+        var canvas = document.getElementById("catChart");
+        var catChart = canvas.getContext("2d");
+        new Chart(catChart).Pie(data);
 	
 }
 
@@ -83,37 +127,3 @@ chooseRight.addEventListener("click", track1.displayWinnerRight);
 document.getElementById("nextButton").addEventListener('click', track1.displayPhotos);
 document.getElementById("nextButton").style.cssText = "visibility: hidden;";
 
-this.Pie = function(data,options){
-
-        chart.Pie.defaults = {
-            segmentShowStroke : true,
-            segmentStrokeColor : "#fff",
-            segmentStrokeWidth : 2,
-            animation : true,
-            animationSteps : 100,
-            animationEasing : "easeOutBounce",
-            animateRotate : true,
-            animateScale : false,
-            onAnimationComplete : null
-        };      
-
-        var config = (options)? mergeChartConfig(chart.Pie.defaults,options) : chart.Pie.defaults;
-
-        return new Pie(data,config,context);                
-    };
-
-var data = [
-
-        {
-            value: 1,
-            color:"#EF6CCD"
-        },
-        {
-            value : 1,
-            color : "#A786CF"
-        }
-
-    ];
-    var canvas = document.getElementById("catChart");
-    var catChart = canvas.getContext("2d");
-    new Chart(catChart).Pie(data);
